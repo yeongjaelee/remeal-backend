9acb996e394bd2fcb587d2b33bbed638a5511d10
yeongjae <yeongjae.blocket@gmail.com>
2023-04-25 02:45:27 +0900

    temp

--- post/types/post_type.py
+++ post/types/post_type.py
@@ -16,6 +16,8 @@ class PostType(DjangoObjectType):
     date_created_year = graphene.String()
     date_created_month = graphene.String()
     date_created_day = graphene.String()
+    date_created_hour = graphene.String()
+    date_created_minute = graphene.String()
     tags_on_post = graphene.List(TagType)
     comments = graphene.List(CommentType)
     @staticmethod
@@ -45,9 +47,24 @@ class PostType(DjangoObjectType):
         return number_with_zeros
     @staticmethod
     def resolve_date_created_day(root, _):
-        if root.date_created.month < 10:
+        if root.date_created.day < 10:
             number_with_zeros = '{:02d}'.format(root.date_created.day)
         else:
             number_with_zeros = root.date_created.day
         return number_with_zeros
 
+    @staticmethod
+    def resolve_date_created_hour(root, _):
+        if root.date_created.hour < 10:
+            number_with_zeros = '{:02d}'.format(root.date_created.hour)
+        else:
+            number_with_zeros = root.date_created.hour
+        return number_with_zeros
+
+    @staticmethod
+    def resolve_date_created_minute(root, _):
+        if root.date_created.minute < 10:
+            number_with_zeros = '{:02d}'.format(root.date_created.minute)
+        else:
+            number_with_zeros = root.date_created.minute
+        return number_with_zeros
