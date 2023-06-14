808c6f0c00487df0ffb285b8664f3a7e577b64ae
yeongjae <yeongjae.blocket@gmail.com>
2023-06-14 10:06:24 +0900

    allowed_host 테스트서버 추가

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = 'django-insecure-=cldztbc4jg&xl0!x673!*v2_=p$$eu)=7*f#d0#zs$44xx-h^
 DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app',
-                 'test.api.re-meal.com']
+                 'test.api.re-meal.com',
+                 '3.38.85.232']
 
 
 # Application definition
