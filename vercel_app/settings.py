f1ec5ecab316b274adf61ef358d8568c95344ac5
yeongjae <yeongjae.blocket@gmail.com>
2023-06-26 23:23:31 +0900

    temp

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -101,7 +101,7 @@ CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
     "http://127.0.0.1:8000",
     "https://kauth.kakao.com",
-    'https://www.re-meal.com/'
+    'https://www.re-meal.com'
 ]
 CORS_ALLOW_METHODS = [
     'GET',
