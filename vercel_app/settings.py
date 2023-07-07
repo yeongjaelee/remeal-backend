160119be8fd62575ceedf936c8c381125f421185
yeongjae <yeongjae.blocket@gmail.com>
2023-07-07 20:41:47 +0900

    temp

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -156,11 +156,11 @@ AUTH_USER_MODEL = 'user.User'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'postgres.csnxoburupwd.ap-northeast-2.rds.amazonaws.com',
-        'PORT': '5432',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT'),
     }
 }
 
