d2e39f14574e97f915f03091d602c6a545d339a4
yeongjae <yeongjae.blocket@gmail.com>
2023-07-09 12:35:08 +0900

    temp

--- user/mutations/kakao_login.py
+++ user/mutations/kakao_login.py
@@ -11,7 +11,7 @@ class KakaoLogin(graphene.Mutation):
     def mutate(cls, _, __):
         client_id = 'f6b986215c4dc13b37c8ca0e4e57ad26'
         kakao_auth_id = "https://kauth.kakao.com/oauth/authorize?response_type=code"
-        redirect_uri = "http://127.0.0.1:8000/kakao-info"
+        redirect_uri = "https://dev.re-meal.com/kakao-info"
 
         return KakaoLogin(success=True,
                           url=f"{kakao_auth_id}&client_id={client_id}&redirect_uri={redirect_uri}"
